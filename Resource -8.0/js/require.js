39f988356d3b43fa177a2c599650501d7ae9f134
sholin0 <18703602360@163.com>
2017-10-25 11:13:46 +0800

    删除

--- Resource -8.0/js/require.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$(".header_load").load("header.html");
-	$(".footer_load").load("footer.html");
-	$(".bread_load").load("bread.html");
-	$(".mo_bread_load").load("mo_bread.html");
-	$(".nav").load("nav.html");
-})
