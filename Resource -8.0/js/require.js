fbea866d2170f2a598d5cdab2d7ef8d1cd146f4e
sholin0 <18703602360@163.com>
2017-10-25 10:57:50 +0800

    jinsai

---
+++ Resource -8.0/js/require.js
@@ -0,0 +1,7 @@
+$(function(){
+	$(".header_load").load("header.html");
+	$(".footer_load").load("footer.html");
+	$(".bread_load").load("bread.html");
+	$(".mo_bread_load").load("mo_bread.html");
+	$(".nav").load("nav.html");
+})
